5724e95a5925a7260649ea634972b43086537202
Hao Ngo <haoxray@gmail.com>
2019-11-28 21:28:49 +0700

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Hao Ngo`,
+        short_name: `Hao Ngo`,
+        start_url: `/`,
+        icon: `src/images/favicon.png`, 
+        lang: `en`,
+        display: `standalone`,
+        crossOrigin: `use-credentials`
+      }
+    },
+    `gatsby-plugin-offline`,
   ],
 }
